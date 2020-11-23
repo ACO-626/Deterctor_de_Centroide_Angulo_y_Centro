835004a6e8b7a387d6276d28e34861ff995787af
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2020-11-23 00:35:48 -0600

    Actualización

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,14 +34,27 @@
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.bordeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.detectarBordeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.detectarCentroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.detectarCentroideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.detectarÁnguloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBorde = new System.Windows.Forms.Button();
+            this.numericBorde2 = new System.Windows.Forms.NumericUpDown();
+            this.numericBorde1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lbNumObj = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBorde2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBorde1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -52,7 +65,8 @@
             this.bordeToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1691, 30);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1331, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -62,20 +76,20 @@
             this.importarToolStripMenuItem,
             this.salirToolStripMenuItem});
             this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
-            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
             this.archivoToolStripMenuItem.Text = "Archivo";
             // 
             // importarToolStripMenuItem
             // 
             this.importarToolStripMenuItem.Name = "importarToolStripMenuItem";
-            this.importarToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.importarToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
             this.importarToolStripMenuItem.Text = "Importar";
             this.importarToolStripMenuItem.Click += new System.EventHandler(this.importarToolStripMenuItem_Click);
             // 
             // salirToolStripMenuItem
             // 
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
-            this.salirToolStripMenuItem.Size = new System.Drawing.Size(150, 26);
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
             this.salirToolStripMenuItem.Text = "Salir";
             this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
@@ -87,72 +101,193 @@
             this.detectarCentroideToolStripMenuItem,
             this.detectarÁnguloToolStripMenuItem});
             this.bordeToolStripMenuItem.Name = "bordeToolStripMenuItem";
-            this.bordeToolStripMenuItem.Size = new System.Drawing.Size(63, 26);
+            this.bordeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
             this.bordeToolStripMenuItem.Text = "Borde";
             // 
             // detectarBordeToolStripMenuItem
             // 
             this.detectarBordeToolStripMenuItem.Name = "detectarBordeToolStripMenuItem";
-            this.detectarBordeToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.detectarBordeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.detectarBordeToolStripMenuItem.Text = "Detectar Borde";
             this.detectarBordeToolStripMenuItem.Click += new System.EventHandler(this.detectarBordeToolStripMenuItem_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(16, 50);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(777, 503);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(868, 50);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(777, 503);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
             // 
             // detectarCentroToolStripMenuItem
             // 
             this.detectarCentroToolStripMenuItem.Name = "detectarCentroToolStripMenuItem";
-            this.detectarCentroToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.detectarCentroToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.detectarCentroToolStripMenuItem.Text = "Detectar Centro";
             this.detectarCentroToolStripMenuItem.Click += new System.EventHandler(this.detectarCentroToolStripMenuItem_Click);
             // 
             // detectarCentroideToolStripMenuItem
             // 
             this.detectarCentroideToolStripMenuItem.Name = "detectarCentroideToolStripMenuItem";
-            this.detectarCentroideToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.detectarCentroideToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.detectarCentroideToolStripMenuItem.Text = "Detectar Centroide";
             // 
             // detectarÁnguloToolStripMenuItem
             // 
             this.detectarÁnguloToolStripMenuItem.Name = "detectarÁnguloToolStripMenuItem";
-            this.detectarÁnguloToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.detectarÁnguloToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.detectarÁnguloToolStripMenuItem.Text = "Detectar Ángulo";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 123);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(583, 409);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(633, 123);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(583, 409);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbNumObj);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.numericBorde1);
+            this.panel1.Controls.Add(this.numericBorde2);
+            this.panel1.Controls.Add(this.btnBorde);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1331, 81);
+            this.panel1.TabIndex = 3;
+            // 
+            // btnBorde
+            // 
+            this.btnBorde.Location = new System.Drawing.Point(25, 3);
+            this.btnBorde.Name = "btnBorde";
+            this.btnBorde.Size = new System.Drawing.Size(76, 75);
+            this.btnBorde.TabIndex = 0;
+            this.btnBorde.Text = "BORDE";
+            this.btnBorde.UseVisualStyleBackColor = true;
+            this.btnBorde.Click += new System.EventHandler(this.btnBorde_Click);
+            // 
+            // numericBorde2
+            // 
+            this.numericBorde2.Location = new System.Drawing.Point(107, 58);
+            this.numericBorde2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericBorde2.Name = "numericBorde2";
+            this.numericBorde2.Size = new System.Drawing.Size(53, 20);
+            this.numericBorde2.TabIndex = 1;
+            this.numericBorde2.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            // 
+            // numericBorde1
+            // 
+            this.numericBorde1.Location = new System.Drawing.Point(107, 32);
+            this.numericBorde1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericBorde1.Name = "numericBorde1";
+            this.numericBorde1.Size = new System.Drawing.Size(53, 20);
+            this.numericBorde1.TabIndex = 2;
+            this.numericBorde1.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(107, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "LÍMITES ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 75);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "CENTROS";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 75);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "ÁNGULO";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1130, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(189, 32);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "CENTRÓIDDE";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(339, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 75);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "CONTAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lbNumObj
+            // 
+            this.lbNumObj.AutoSize = true;
+            this.lbNumObj.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNumObj.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbNumObj.Location = new System.Drawing.Point(421, 22);
+            this.lbNumObj.Name = "lbNumObj";
+            this.lbNumObj.Size = new System.Drawing.Size(41, 31);
+            this.lbNumObj.TabIndex = 8;
+            this.lbNumObj.Text = "---";
             // 
             // FormPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(18)))));
-            this.ClientSize = new System.Drawing.Size(1691, 751);
+            this.ClientSize = new System.Drawing.Size(1331, 610);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormPrincipal";
             this.Text = "Detector de contorno";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBorde2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericBorde1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,6 +306,16 @@
         private System.Windows.Forms.ToolStripMenuItem detectarCentroToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem detectarCentroideToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem detectarÁnguloToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBorde;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericBorde1;
+        private System.Windows.Forms.NumericUpDown numericBorde2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbNumObj;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,15 @@ namespace Deterctor_de_Contornos
 {
     public partial class FormPrincipal : Form
     {
-
-        Image<Bgr, byte> imgInput;
+        //Imagen original en color
+        Image<Bgr, byte> img;
+        //Matriz que almacena 0 y 1 dependiendo del color de la imagen de contornos binarizada
         int[,] matrizImagen;
+        //Imagen binarizada
+        Image<Gray, byte> GrayImg;
+        //Imagen que se muestra
+        Image<Gray, byte> imgout;
+
 
         public FormPrincipal()
         {
@@ -34,30 +40,47 @@ namespace Deterctor_de_Contornos
             OpenFileDialog ofd = new OpenFileDialog();
             if(ofd.ShowDialog() == DialogResult.OK)
             {
-                imgInput = new Image<Bgr, byte>(ofd.FileName);
-                pictureBox1.Image = imgInput.ToBitmap();
+                img = new Image<Bgr, byte>(ofd.FileName);
+                pictureBox1.Image = img.ToBitmap();
             }
         }
 
+
+        #region DetectarBorde
+        private void detectarBorde()
+        {
+            
+                if(img!=null)
+                {
+                    GrayImg = img.Convert<Gray, byte>().ThresholdBinary(new Gray((int)numericBorde1.Value), new Gray((int)numericBorde2.Value));
+                    Emgu.CV.Util.VectorOfVectorOfPoint contours = new Emgu.CV.Util.VectorOfVectorOfPoint();
+                    Mat hier = new Mat();
+
+                    imgout = new Image<Gray, byte>(img.Width, img.Height, new Gray(0));
+
+                    CvInvoke.FindContours(GrayImg, contours, hier, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+                    CvInvoke.DrawContours(imgout, contours, -1, new MCvScalar(255, 255, 255));
+
+                    pictureBox2.Image = imgout.Bitmap;
+                }else
+                {
+                    MessageBox.Show("Debe cargar una imagen antes de buscar el borde", "Sin imagen", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                
+     
+        }
+
+        //Botón
+        private void btnBorde_Click(object sender, EventArgs e)
+        {
+            detectarBorde();
+        }
+        //Borde en Menú
         private void detectarBordeToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
             detectarBorde();
 
-        }
-        #region DetectarBorde
-        private void detectarBorde()
-        {
-            Image<Gray, byte> imgOutput = imgInput.Convert<Gray, byte>().ThresholdBinary(new Gray(100), new Gray(255));
-            Emgu.CV.Util.VectorOfVectorOfPoint contours = new Emgu.CV.Util.VectorOfVectorOfPoint();
-            Mat hier = new Mat();
-
-            Image<Gray, byte> imgout = new Image<Gray, byte>(imgInput.Width, imgInput.Height, new Gray(0));
-
-            CvInvoke.FindContours(imgOutput, contours, hier, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
-            CvInvoke.DrawContours(imgout, contours, -1, new MCvScalar(255, 255,255));
-
-            pictureBox2.Image = imgout.Bitmap;
         }
         #endregion
 
@@ -70,7 +93,52 @@ namespace Deterctor_de_Contornos
 
         private void detectarCentro()
         {
+            matrizImagen = new int[img.Width, img.Height];
+            if(img!=null)
+            {
+                for(int i = 0;i<img.Width;i++)
+                {
+                    for(int j=0;j<img.Height;j++)
+                    {
+                        if (imgout[i, j].Intensity == 0)
+                            matrizImagen[i, j] = 0;                              
+                        
+                    }
+                }
+            }
 
+
+
+            //Representación del contenido de la matriz
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    if (matrizImagen[i,j]==0)
+                    {
+                        imgout[i, j] = new Gray(0);
+                    }else
+                    {
+                        imgout[i, j] = new Gray(255);
+                    }
+
+                }
+            }
+            pictureBox2.Image = imgout.Bitmap;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void contarObjetos()
+        {
+            int numero = 0;
+            
+
+            //findContours(CHAIN_APPROX_METHOD.CV_CHAIN_APPROX_SIMPLE, RETR_TYPE.CV_RETR_EXTERNAL);
         }
     }
 }
